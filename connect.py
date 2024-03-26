c20c1856260fb61f446b98d9ce72dfd064423e53
Audrey Fuller <alf9310@g.rit.edu>
2024-03-26 13:36:18 -0400

    users started, getting wierd error with primary key not being recognised

--- connect.py
+++ connect.py
@@ -2,6 +2,11 @@ import os
 import psycopg2
 from sshtunnel import SSHTunnelForwarder
 from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import MetaData
+
+#from user import User
 
 load_dotenv()
 
@@ -20,14 +25,57 @@ try:
             'database': DB_NAME,
             'user': USERNAME,
             'password': PASSWORD,
-            'host': 'localhost',
+            'host': '127.0.0.1',
             'port': server.local_bind_port
         }
-
 
         conn = psycopg2.connect(**params)
         curs = conn.cursor()
         print("Database connection established")
+
+        # Build SQLAlchemy database URL with SSH tunnel
+        db_url = f'postgresql://{USERNAME}:{PASSWORD}@127.0.0.1:{server.local_bind_port}/{DB_NAME}'
+
+        # Create SQLAlchemy engine and session
+        engine = create_engine(db_url)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        # Check user columns
+        metadata = MetaData()
+        metadata.reflect(bind=engine) 
+        print(metadata.tables.keys())  # Print table names
+        print(metadata.tables['users'].columns)  # Print table structure for 'users' table
+
+        '''
+        # Use the session to interact with the database using SQLAlchemy
+        # ----------------Testing User usage----------------
+
+        # Create a user
+        new_user = User.create(session, 1000000, "John", "Doe", "JohnnyBoy", "password123")
+
+        # Query users
+        # Call the search method with the provided session and first_name parameter
+        results, total_count = User.search(session, {}, first_name="John")
+
+        # Print the results
+        for user in results:
+            print(user.first_name, user.last_name)
+
+        # Update a user
+        new_user.last_name = "Smith"
+        new_user.save()
+
+        # Delete a user
+        new_user.delete()
+
+        # Call the search method with the provided session and first_name parameter
+        results, total_count = User.search(session, {}, first_name="John")
+
+        # Print the results
+        for user in results:
+            print(user.first_name, user.last_name)
+        '''
 
         # # database work
 
