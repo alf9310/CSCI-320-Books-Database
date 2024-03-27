2c05a0ade7d7605adb9ee8c29559414abc85b265
tusmm <r.ong40915@gmail.com>
2024-03-27 09:41:59 -0400

    search by genere

--- application/main.py
+++ application/main.py
@@ -7,7 +7,7 @@ def main():
     # search.search_by_length()
     #search.search_by_min_age()
     #search.search_by_max_age()
-    search.search_by_author()
-
+    # search.search_by_author()
+    search.search_by_genre()
 if __name__ == "__main__":
     main()
--- application/search.py
+++ application/search.py
@@ -128,12 +128,69 @@ def search_by_published() -> None:
     print("get published")
 
 
+
+def search_by_edited() -> None:
+    print("search by editedb")
+
+
+# future work, make this able to take in a list? 
 def search_by_genre() -> None:
-    print("search genre")
+    is_still_searching = True
+    while(is_still_searching):
+        genre = utils.get_input_str("What is a book genre you are lookiing for?\n-> ")
+        
+        print(f'Your search was, {genre}\n')
+
+        # from person list, find the person that is most like the inputted author
+        # then use the gotten persons to link them to the books they wrote
+        query_statement = f"\
+            SELECT book.title, genre.genre_name \
+            FROM book \
+            INNER JOIN has_genre \
+            ON book.bid = has_genre.bid \
+            INNER JOIN genre \
+            ON genre.gid = has_genre.gid \
+            WHERE genre.genre_name ILIKE '%{genre}%' \
+            ORDER BY genre.genre_name"
+        query_results = connect.execute_query(query_statement) 
+        if query_results: 
+            for results in query_results:
+                print(f'{results[1]}:  {results[0]}')
+        else:
+            print("No results")
+        
+        prompt = "Want to keep searching?"
+        is_still_searching = utils.ask_continue(prompt) 
 
 
 def search_by_rates() -> None:
     print("search rates")
+    # is_still_searching = True
+    # while(is_still_searching):
+    #     author_name = utils.get_input_str("What is/are the book(s) by some author that you are looking for?\n-> ")
+        
+    #     print(f'Your search was, {author_name}\n')
+
+    #     # from person list, find the person that is most like the inputted author
+    #     # then use the gotten persons to link them to the books they wrote
+    #     query_statement = f"\
+    #         SELECT book.title, person.person_name \
+    #         FROM book \
+    #         INNER JOIN written_by \
+    #         ON book.bid = written_by.bid \
+    #         INNER JOIN person \
+    #         ON person.pid = written_by.pid \
+    #         WHERE person_name ILIKE '%{author_name}%' \
+    #         ORDER BY person_name"
+    #     query_results = connect.execute_query(query_statement) 
+    #     if query_results: 
+    #         for results in query_results:
+    #             print(f'{results[0]} by {results[1]}')
+    #     else:
+    #         print("No results")
+        
+    #     prompt = "Want to keep searching?"
+    #     is_still_searching = utils.ask_continue(prompt) 
 
 
 # future work, a generic filter function
