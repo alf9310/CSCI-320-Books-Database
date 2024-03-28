897762f3713245c3b2bf5a3ce1583ba429be71f7
tgk1703 <tgk1703@g.rit.edu>
2024-03-28 11:33:40 -0400

    Can search for friends via email or username

--- application/friend.py
+++ application/friend.py
@@ -11,31 +11,47 @@ from users import Users
 Base = declarative_base()
 
 class Friend(Base):
+    '''
+    Defines friend
+    '''
     __tablename__ = 'friend'
     uid = Column(Integer, primary_key=True)
     friend_id = Column(Integer, primary_key=True)
-
+    '''
+    friend constructor
+    '''
     def __init__(self, uid, friend_id):
         self.uid = uid
         self.friend_id = friend_id
-
+    '''
+    To-string for friend
+    '''
     def __str__(self):
         return (f"friend(uid='{self.uid}', friend_id='{self.friend_id}')")
     
     '''
-    TODO need to be able to unfollow/unfriend users. Need to be able to search for users
-    to friend via email. Will still keep friend search via username because that seems useful.
+    Creates a new entry in the friend table where the current user is the uid and
+    the person they are following is the friend_id
     '''
-    
     @classmethod
     def friend_user(cls, session, uid):
-        # Need to update to allow search by email
         friending = True
+        uore = True
         
         while(friending):
+            while(uore):
+                user_or_email = input("Do you want to search by username or by email? ")
+                if(user_or_email != "email" and user_or_email != "username"):
+                    print("Please specify either username or email")
+                else:
+                    uore = False
+
             friending = input("Who do you want to friend? ")
             try:
-                friendResult, fcount = Users.search(session, username=friending)
+                if(user_or_email == "email"):
+                    friendResult, fcount = Users.search(session, email=friending)
+                elif (user_or_email=="username"):
+                    friendResult, fcount = Users.search(session, username=friending)
                 reply = input("Do you want to friend " + friendResult[0].username + "? [Y/N] ")
                 if(reply == "Y" or reply == "YES"):
                     new_friend = cls(uid = uid, friend_id = friendResult[0].uid)
@@ -49,12 +65,25 @@ class Friend(Base):
             again = input("Do you want to friend someone else? [Y/N] ")
             if(again=="N" or again=="NO"):
                 friending = False
-
+    '''
+    Deletes an entry in the friend table where the current user is
+    the uid and the person they are unfollowing is the friend_id
+    '''
     @classmethod
     def unfriend(cls, session, uid):
+        uore = True
+        while(uore):
+                user_or_email = input("Do you want to search by username or by email? ")
+                if(user_or_email != "email" and user_or_email != "username"):
+                    print("Please specify either username or email")
+                else:
+                    uore = False
         unfriending = input("Who would you like to unfriend? ")
         try:
-            friendresult, fcount = Users.search(session, username=unfriending)
+            if(user_or_email == "email"):
+                friendresult, fcount = Users.search(session, email=unfriending)
+            elif(user_or_email == "username"):
+                friendresult, fcount = Users.search(session, username=unfriending)
             fid = friendresult[0].uid
             confirm = input("Do you wish to unfriend " + friendresult[0].username + " ? [Y/N] ")
             if(confirm == "Y" or confirm == "YES"):
