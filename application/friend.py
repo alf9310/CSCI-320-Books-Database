be63bcd46bb6c9346686f2c707c0d56208196ed6
tgk1703 <tgk1703@g.rit.edu>
2024-04-18 14:35:58 -0400

    Can see number of followers/following

--- application/friend.py
+++ application/friend.py
@@ -106,3 +106,8 @@ class Friend(Base):
             for entry in query:
                 friendName, fcount = Users.search(session, uid=entry.friend_id)
                 print(friendName[0].username)
+        print("\nFolliwng: " + str(count))
+        queryFollow = session.query(Friend)
+        queryFollow = queryFollow.filter(Friend.friend_id == uid)
+        countFollow = queryFollow.count()
+        print("Followers: " + str(countFollow))
