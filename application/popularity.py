5b779e0438827b5390a155beba4b4f8d24934c60
clh5602 <clh5602@rit.edu>
2024-04-18 14:42:25 -0400

    90 day trend complete

--- application/popularity.py
+++ application/popularity.py
@@ -9,11 +9,10 @@ Base = declarative_base()
 
 '''
 Calculates the "popularity" value of a given book.
-Popularity = avg_rating^2 * number_of_recent_logs
+Popularity = avg_rating^3 * number_of_recent_logs
 '''
 def calculate_popularity( log_count, avg_rating ):
-
-    return log_count * avg_rating * avg_rating
+    return log_count * avg_rating * avg_rating * avg_rating
 
 '''
 Displays the top 20 books from the past
@@ -21,7 +20,7 @@ Displays the top 20 books from the past
 '''
 def recently_popular_books( session ):
     
-    print("Please hold...")
+    print("Calculating popularity...")
     
     # Create a blank array for the books and their popularity
     arr = [[-1 for i in range(2)] for j in range(20)]
@@ -30,34 +29,33 @@ def recently_popular_books( session ):
     time_filter = datetime.now() - timedelta(days=90)
 
     # Get logs within the past 90 days
-    #log_arr, log_count = Log.search(session=session, start_time=time_filter, order_by="bid")
     book_occurrence = session.execute(text(f"""SELECT bid, COUNT(*) AS log_count
     FROM logs
     WHERE start_time >= '{time_filter.strftime('%Y-%m-%d %H:%M:%S')}'
-    GROUP BY bid"""))
+    GROUP BY bid""")).all()
 
-    for i in range(len(book_occurrence[0])):
+    for i in range(len(book_occurrence)):
 
         # Get the avg
-        avg_rating = session.query(func.avg(Rates.rating).label('average')).filter(Rates.bid==book_occurrence[0][i])      
+        avg_rating = session.query(func.avg(Rates.rating).label('average')).filter(Rates.bid==book_occurrence[i][0])     
         avg_rating = session.execute(avg_rating).first()[0]
         
-        popularity = calculate_popularity(book_occurrence[1][i], avg_rating)
+        popularity = calculate_popularity(book_occurrence[i][1], avg_rating)
 
         # arr holds the top 20 books in order,
         # column 1 is the book id,
         # column 2 is the popularity rating
 
         # goes from index 0 to index 19
-        for i in range(20):
-            if (popularity > arr[i][1]):
-                if (i > 0):
+        for k in range(20):
+            if (popularity > arr[k][1]):
+                if (k > 0):
                     # move the current one back
-                    arr[i-1][0] = arr[i][0]
-                    arr[i-1][1] = arr[i][1]
+                    arr[k-1][0] = arr[k][0]
+                    arr[k-1][1] = arr[k][1]
                 # replace
-                arr[i][0] = book_occurrence[0][i]
-                arr[i][1] = popularity
+                arr[k][0] = book_occurrence[i][0]
+                arr[k][1] = popularity
             else:
                 break
 
